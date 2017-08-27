d951b34be26f0a3ed31e6801ea9b35ec64506678
8845musign <8845musign@gmail.com>
2017-08-27 18:50:02 +0900

    add async increment

--- sagas.js
+++ sagas.js
@@ -1,3 +1,26 @@
+import { delay } from 'redux-saga'
+import { put, takeEvery, all } from 'redux-saga/effects'
+
 export function* helloSaga() {
   console.log('Hello Sagas!')
+}
+
+export function* incrementAsync() {
+  yield delay(1000)
+  yield put({ type: 'INCREMENT' })
+}
+
+export function* waitIncrementAsync() {
+  yield takeEvery('INCREMENT_ASYNC', incrementAsync)
+}
+
+export function* watchIncrementAsync() {
+  yield takeEvery('INCREMENT_ASYNC', incrementAsync)
+}
+
+export default function* rootSaga() {
+  yield all([
+    helloSaga(),
+    watchIncrementAsync()
+  ])
 }
